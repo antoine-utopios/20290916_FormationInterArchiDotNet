37dbaca61a8de835fbfaaf67e9324ed97776631e
Antoine D. <antoine@utopios.net>
2026-09-16 14:42:30 +0200

    Ajout du projet de l'API sans utilisation de Minimal API

---
+++ Project3.WebAPIControllers/Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Project3.WebAPIControllers.Models;
+
+namespace Project3.WebAPIControllers.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ClientController : ControllerBase
+{
+    private List<Client> clientLists;
+    private int clientCounter;
+
+    public ClientController()
+    {
+        clientLists = new()
+        {
+          new Client() { Id = ++clientCounter, Firstname = "John", Lastname = "DUPONT", Email = "j.dupont@example.com"},
+          new Client() { Id = ++clientCounter, Firstname = "Martha", Lastname = "DOE", Email = "m.doe@example.com"},
+          new Client() { Id = ++clientCounter, Firstname = "Clark", Lastname = "SMITH", Email = "c.smith@example.com"}
+        };
+    }
+
+    [HttpGet(Name = "GetAllClients")]
+    public ActionResult<List<Client>> GetAllClients()
+    {
+        return Ok(clientLists);
+    }
+
+    [HttpGet("{clientId:int}", Name = "GetClientById")]
+    public ActionResult<Client?> GetClientById(int clientId)
+    {
+        var clientFound = clientLists.FirstOrDefault(x => x.Id == clientId);
+
+        if (clientFound is null) return NotFound();
+        else return Ok(clientFound);
+    }
+}
---
+++ Project3.WebAPIControllers/Models/Client.cs
@@ -0,0 +1,9 @@
+namespace Project3.WebAPIControllers.Models;
+
+public class Client
+{
+    public int Id { get; set; } = 0;
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
